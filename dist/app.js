cf5181e452cc89bce1a303bc5466abf2049ba4fd
SIDIDEV <sidimariko61@gmail.com>
2024-04-22 18:12:54 +0100

    Readonly

--- dist/app.js
+++ dist/app.js
@@ -1,27 +1,4 @@
 "use strict";
-const compteur = document.querySelector("#compteur");
-let i = 0;
-const increment = (e) => {
-    e.preventDefault();
-    i++;
-    const span = compteur.querySelector("span");
-    span ? (span.innerHTML = i.toString()) : "";
-};
-compteur.addEventListener("click", increment);
-const user = {
-    name: "ok",
-    age: 10,
-};
-function identity(arg) {
-    return arg;
+function reverse(arg) {
+    return [...arg].reverse();
 }
-const aa = identity(3);
-function tablee(arg) {
-    return arg[0];
-}
-const aaa = tablee(["hellow", 1, false]);
-function namefn(arg) {
-    console.log(arg.length);
-    return arg;
-}
-const aasa = namefn([3, ""]);
