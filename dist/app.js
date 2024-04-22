7e9efb04d74bb0676a03db252b248b8f144547ad
SIDIDEV <sidimariko61@gmail.com>
2024-04-22 17:54:27 +0100

    Alias and generics

--- dist/app.js
+++ dist/app.js
@@ -8,3 +8,20 @@ const increment = (e) => {
     span ? (span.innerHTML = i.toString()) : "";
 };
 compteur.addEventListener("click", increment);
+const user = {
+    name: "ok",
+    age: 10,
+};
+function identity(arg) {
+    return arg;
+}
+const aa = identity(3);
+function tablee(arg) {
+    return arg[0];
+}
+const aaa = tablee(["hellow", 1, false]);
+function namefn(arg) {
+    console.log(arg.length);
+    return arg;
+}
+const aasa = namefn([3, ""]);
--- src/app.ts
+++ src/app.ts
@@ -9,3 +9,32 @@ const increment = (e: Event) => {
 };
 
 compteur.addEventListener("click", increment);
+
+type User = {
+  name: string;
+  age: number;
+};
+
+const user: User = {
+  name: "ok",
+  age: 10,
+};
+
+function identity<ArgType>(arg: ArgType): ArgType {
+  return arg;
+}
+
+const aa = identity<number>(3);
+
+function tablee<Type>(arg: Type[]): Type {
+  return arg[0];
+}
+
+const aaa = tablee(["hellow", 1, false]);
+
+function namefn<Type extends { length: number }>(arg: Type): Type {
+  console.log(arg.length);
+  return arg;
+}
+
+const aasa = namefn([3, ""]);
