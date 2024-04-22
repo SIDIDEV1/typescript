4dc84cdb49fce7a5548205979d818f7bc3defc81
SIDIDEV <sidimariko61@gmail.com>
2024-04-22 16:56:08 +0100

    Global typed in typescript

--- dist/app.js
+++ dist/app.js
@@ -1,7 +1,24 @@
-const compteur = document.querySelector("#compteur");
+"use strict";
+/*const compteur = document.querySelector("#compteur");
 let i = 0;
+
 const increment = (e) => {
-    i++;
-    compteur.querySelector("span").innerHTML = i.toString();
+  i++;
+  compteur.querySelector("span").innerHTML = i.toString();
 };
+
 compteur.addEventListener("click", increment);
+*/
+const a = "helloww";
+const n = 1;
+const b = true;
+const arr = ["ok", "ok"];
+const all = ["ok", 1, []];
+const obj = { age: 10 };
+const user = {
+    name: "ok",
+    age: 10,
+};
+const date = new Date();
+const compteur = document.querySelector("#compteur");
+const cb = (e) => { };
--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-const compteur = document.querySelector("#compteur");
+/*const compteur = document.querySelector("#compteur");
 let i = 0;
 
 const increment = (e) => {
@@ -7,3 +7,20 @@ const increment = (e) => {
 };
 
 compteur.addEventListener("click", increment);
+*/
+
+const a: string = "helloww";
+const n: number = 1;
+const b: boolean = true;
+const arr: string[] = ["ok", "ok"];
+const all: any[] = ["ok", 1, []];
+const obj: { name?: string; age: number } = { age: 10 };
+const user: { name: string; [key: string]: string | number } = {
+  name: "ok",
+  age: 10,
+};
+const date: Date = new Date();
+
+const compteur = document.querySelector("#compteur") as HTMLElement;
+
+const cb: Function = (e: MouseEvent): void => {};
