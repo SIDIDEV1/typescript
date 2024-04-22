2e2c7451d26611866b4d8f406afebad9fcec37b4
SIDIDEV <sidimariko61@gmail.com>
2024-04-22 17:14:21 +0100

    typed my counteur code

--- dist/app.js
+++ dist/app.js
@@ -1,24 +1,10 @@
 "use strict";
-/*const compteur = document.querySelector("#compteur");
-let i = 0;
-
-const increment = (e) => {
-  i++;
-  compteur.querySelector("span").innerHTML = i.toString();
-};
-
-compteur.addEventListener("click", increment);
-*/
-const a = "helloww";
-const n = 1;
-const b = true;
-const arr = ["ok", "ok"];
-const all = ["ok", 1, []];
-const obj = { age: 10 };
-const user = {
-    name: "ok",
-    age: 10,
-};
-const date = new Date();
 const compteur = document.querySelector("#compteur");
-const cb = (e) => { };
+let i = 0;
+const increment = (e) => {
+    e.preventDefault();
+    i++;
+    const span = compteur.querySelector("span");
+    span ? (span.innerHTML = i.toString()) : "";
+};
+compteur.addEventListener("click", increment);
--- src/app.ts
+++ src/app.ts
@@ -1,26 +1,11 @@
-/*const compteur = document.querySelector("#compteur");
+const compteur = document.querySelector("#compteur") as HTMLElement;
 let i = 0;
 
-const increment = (e) => {
+const increment = (e: Event) => {
+  e.preventDefault();
   i++;
-  compteur.querySelector("span").innerHTML = i.toString();
+  const span = compteur.querySelector("span");
+  span ? (span.innerHTML = i.toString()) : "";
 };
 
 compteur.addEventListener("click", increment);
-*/
-
-const a: string = "helloww";
-const n: number = 1;
-const b: boolean = true;
-const arr: string[] = ["ok", "ok"];
-const all: any[] = ["ok", 1, []];
-const obj: { name?: string; age: number } = { age: 10 };
-const user: { name: string; [key: string]: string | number } = {
-  name: "ok",
-  age: 10,
-};
-const date: Date = new Date();
-
-const compteur = document.querySelector("#compteur") as HTMLElement;
-
-const cb: Function = (e: MouseEvent): void => {};
